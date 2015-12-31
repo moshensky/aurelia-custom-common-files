17cd4c788cac0651d0a6982c83bc77d6f0b5a64f
Nikita Moshensky <nikita@moshensky.com>
2015-12-31 09:15:25 +0200

    build source

--- dist/es6/index.js
+++ dist/es6/index.js
@@ -11,8 +11,6 @@ export function configure(aurelia, configCallback) {
   aurelia.globalResources('./custom-elements/timepicker/timepicker');
   aurelia.globalResources('./custom-elements/datepicker/datepicker');
   aurelia.globalResources('./custom-elements/datetimepicker/datetimepicker');
-  aurelia.globalResources('./custom-elements/select2/select2');
-  aurelia.globalResources('./custom-elements/select2-ajax/select2-ajax');
   aurelia.globalResources('./custom-attributes/enable');
 
   aurelia.globalResources('./value-converters/date-format');
